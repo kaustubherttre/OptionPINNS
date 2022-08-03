e6daca321192673677bbf65f7d16c0e28a6b6ce9
Kaustubh <kaustubh@PWLAP2406.Etechaces.com>
2022-08-03 17:46:14 +0530

    Complete BS Numerical

--- BSNumerical.py
+++ BSNumerical.py
@@ -1,14 +1,9 @@
 from math import *
 import numpy as np
 import pandas as pd
-
+import seaborn as sns
+import matplotlib.pyplot as plt 
 # The mesh is defined as bt S and T, S is defined on the Y-axis and T is given on the x axis. 
-# T
-
-#aa=0.5*dt*(sigma*sigma*(1:N-2).*(1:N-2)-r*(1:N-2))';
-#bb=1-dt*(sigma*sigma*(1:N-2).*(1:N-2)+r)';
-#cc=0.5*dt*(sigma*sigma*(1:N-2).*(1:N-2)+r*(1:N-2))';
-#v(2:N-1,i)=bb.*v(2:N-1,i-1)+cc.*v(3:N,i-1)+aa.*v(1:N-2,i-1)
 class BSNumerical:
     def __init__(self, Smin, Smax, K, T, r, sigma, n_shares, n_time):
         self.Smin = Smin
@@ -40,17 +35,27 @@ class BSNumerical:
                 t_2.append(1 - dt*(pow(sigma,2)*pow(i,2) + r))
                 t_3.append(0.5*dt*(pow(sigma,2)*pow(i,2)+r*i))
             return np.array(t_1), np.array(t_2), np.array(t_3)
+        
         self.t_1, self.t_2, self.t_3  = numerical_equation_helper(self.dt,self.sigma,self.r,self.N)
+
         def finite_difference_mat(N, M, t_1, t_2, t_3, mat):
-            #v(2:N-1,i)=bb.*v(2:N-1,i-1)+cc.*v(3:N,i-1)+aa.*v(1:N-2,i-1)
-            
-            for i in range(1, N-1):
-                for j in range(1,M):
-                    mat[i+1,j-1] = np.dot(mat[1:N-1, j-1], t_2) 
-            return mat
+            for j in range(1,M):
+                mat[1:N-1, j] = np.multiply(t_2, mat[1:N-1, j-1]) + np.multiply(t_3, mat[2:N, j-1]) + np.multiply(t_1, mat[:N-2, j-1])
+            return np.fliplr(mat)
+        
         self.matrix = finite_difference_mat(self.N, self.M, self.t_1, self.t_2, self.t_3, self.mat)
+        def handlePlots(matrix, M):
+            plt.plot(matrix[:,1], label = 't = 0')#t = 0
+            plt.plot(matrix[:,M-1], label = 't = T')
+            plt.legend()
+            plt.show()
+        handlePlots(self.matrix, self.M)
 if __name__ == '__main__':
-    model = BSNumerical(0,20,10,1,0.2,0.25, 5, 5)
-    print(model.mat)
+    model = BSNumerical(0,20,10,1,0.2,0.25, 100, 1000)
+    print(model)
+    #sns.scatterplot()
+    #mat = np.fliplr(model.matrix)
+    
+    
 
 
